273c6187986f098f48c57eca6db0751fcf91940b
Siddesh <x22160345@student.ncirl.ie>
2023-04-24 04:04:33 +0530

    initial commit

---
+++ jobs/choices.py
@@ -0,0 +1,11 @@
+location_choices = {
+    "Mumbai": "Mumbai",
+    "Pune": "Pune",
+    "Bangalore": "Bangalore",
+}
+
+contract_choices = {
+    "Full Time": "Full Time",
+    "Part Time": "Part Time",
+    "Hourly": "Hourly"
+}
